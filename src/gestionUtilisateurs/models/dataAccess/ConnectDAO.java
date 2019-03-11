ee1d1b107a76db2f277a146f5003dc3a7207acf9
Vincent CABILLIC <vcabilli@sio.chaptalqper>
2019-03-11 11:53:26 +0100

    Ajout des fonctions de gestion de connexion

--- src/gestionUtilisateurs/models/dataAccess/ConnectDAO.java
+++ src/gestionUtilisateurs/models/dataAccess/ConnectDAO.java
@@ -1,5 +1,36 @@
 package gestionUtilisateurs.models.dataAccess;
 
-public class ConnectDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectDAO {
+	private static String url = "jdbc:mysql://localhost/gsb-frais";
+	
+	private static String user = "ts2";
+	
+	private static String passwd = "ts2";
+	
+	private static Connection connect = null;
+	
+	public static Connection getInstance(){
+		if (connect == null){
+			try{
+				connect = DriverManager.getConnection(url, user, passwd);
+			}catch (SQLException e){
+				e.printStackTrace();
+			}
+		}
+		return connect;
+	}
+	
+	public static void close(){
+		if(connect != null){
+			try{
+				connect.close();
+			}catch (SQLException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
