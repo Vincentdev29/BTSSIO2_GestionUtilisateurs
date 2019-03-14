ac11017056d3452f9fe97755b109ebc11879da47
Vincent CABILLIC <vcabilli@sio.chaptalqper>
2019-03-14 14:20:17 +0100

    Création javadoc UtilisateurDAO

--- src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
+++ src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
@@ -2,6 +2,15 @@ package gestionUtilisateurs.models.dataAccess;
 
 import java.sql.Connection;
 
+/**
+ * Classe technique permettant le mapping entre les instances d'Utilisateur
+ * et la table utilisateur dans BDD
+ * Définit les opérations autorisées : Create, Read, et Update (CRU)
+ * Elle modélise un objet d'accès aux données (Data Access Object)
+ * 
+ * @author Zanati
+ *
+ */
 public class UtilisateurDAO {
 	
 	public Connection connect;
@@ -10,6 +19,12 @@ public class UtilisateurDAO {
 		super();
 	}
 	
+	/**
+	 * Méthode d'initialisation de la connection à la base de données
+	 * 
+	 * @param userName pseudo de l'utilisateur
+	 * @param userPassword mot de passe de l'utilisateur
+	 */
 	public void connectDatabase(String userName, String userPassword){
 		ConnectDAO.setCredentials(userName, userPassword);
 		connect = ConnectDAO.getInstance();
