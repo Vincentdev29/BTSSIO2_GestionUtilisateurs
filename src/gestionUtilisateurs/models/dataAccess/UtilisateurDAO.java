3a5f553e49f96fd31da5e3dc0dafcd9beccb093b
Vincent Cabillic <vincent.cabillic@hotmail.fr>
2019-03-15 19:58:46 +0100

    Correction typographique UtilisateurDAO

--- src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
+++ src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
@@ -103,44 +103,42 @@ public class UtilisateurDAO {
 	 * @param id identifant de l'utilsateur à rechercher dans la BdD
 	 * @return un objet Utilisateur ou null
 	 */
-		public Utilisateur findById(String id) {
-
-			Utilisateur unUtilisateur = null;
-			try {
-				PreparedStatement statement = connect.prepareStatement("select * from utilisateur where id =  ?");
-				statement.setString(1, id);
-				ResultSet result = statement.executeQuery();
-				if (result.first())
-					unUtilisateur = new Utilisateur(id, result.getString("nom"), result.getString("prenom"));
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return unUtilisateur;
+	public Utilisateur findById(String id) {
+		Utilisateur unUtilisateur = null;
+		try {
+			PreparedStatement statement = connect.prepareStatement("select * from utilisateur where id =  ?");
+			statement.setString(1, id);
+			ResultSet result = statement.executeQuery();
+			if (result.first())
+				unUtilisateur = new Utilisateur(id, result.getString("nom"), result.getString("prenom"));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		return unUtilisateur;
+	}
 		
-		/**
-		 * @param filtreNom nom de l'utilisateur à rechercher dans la BdD
-		 * @return une liste d'objets Utilisateur 
-		 */
-		public List<Utilisateur> findByNom(String filtreNom) {
-
-			Utilisateur unUtilisateur = null;
-			List<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
-			try {
-				PreparedStatement statement = connect.prepareStatement("select * from visiteur where nom like  ?");
-				statement.setString(1, filtreNom);
-				ResultSet result = statement.executeQuery();
-				while (result.next()) {
-					System.out.println(result.getString("id"));
-					unUtilisateur = new Utilisateur(result.getString("id"), result.getString("nom"),
-							result.getString("prenom"));
-					lesUtilisateurs.add(unUtilisateur);
-				}
-
-			} catch (SQLException e) {
-				e.printStackTrace();
+	/**
+	 * @param filtreNom nom de l'utilisateur à rechercher dans la BdD
+	 * @return une liste d'objets Utilisateur 
+	 */
+	public List<Utilisateur> findByNom(String filtreNom) {
+		
+		Utilisateur unUtilisateur = null;
+		List<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
+		try {
+			PreparedStatement statement = connect.prepareStatement("select * from visiteur where nom like  ?");
+			statement.setString(1, filtreNom);
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				System.out.println(result.getString("id"));
+				unUtilisateur = new Utilisateur(result.getString("id"), result.getString("nom"),
+				result.getString("prenom"));
+				lesUtilisateurs.add(unUtilisateur);
 			}
-			return lesUtilisateurs;
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		return lesUtilisateurs;
+	}
 }
