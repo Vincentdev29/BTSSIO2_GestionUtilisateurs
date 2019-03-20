85d1af098e6944974a4e68b24b80ac63aca4e154
Vincent CABILLIC <vcabilli@sio.chaptalqper>
2019-03-20 16:57:36 +0100

    Création méthode retournant un utilisateur contenant ses infos

--- src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
+++ src/gestionUtilisateurs/models/dataAccess/UtilisateurDAO.java
@@ -107,6 +107,19 @@ public class UtilisateurDAO {
 	 */
 	public Utilisateur findByIdInfoUtilisateur(String id){
 		Utilisateur unUtilisateur = null;
+		try{
+			String query = "select id, nom, prenom, adresse, cp, ville, dateEmbauche, statut "
+					+ "from visiteur "
+					+ "where id = ?";
+			PreparedStatement statement = connect.prepareStatement(query);
+			statement.setString(1, id);
+			ResultSet result = statement.executeQuery();
+			if (result.first()){
+					unUtilisateur = new Utilisateur(id, result.getString("nom"), result.getString("prenom"), result.getString("adresse"), result.getString("cp"), result.getString("ville"), result.getString("dateEmbauche"), result.getString("statut"));
+			}
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
 		return unUtilisateur;
 	}
 	
@@ -117,7 +130,7 @@ public class UtilisateurDAO {
 	public Utilisateur findById(String id) {
 		Utilisateur unUtilisateur = null;
 		try {
-			PreparedStatement statement = connect.prepareStatement("select * from utilisateur where id =  ?");
+			PreparedStatement statement = connect.prepareStatement("select * from visiteur where id =  ?");
 			statement.setString(1, id);
 			ResultSet result = statement.executeQuery();
 			if (result.first())
