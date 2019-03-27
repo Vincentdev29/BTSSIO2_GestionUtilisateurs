b3050e6a94d31114d9bc61e93b57efac3cb1d568
Aubry LEDUCQ <aleducq@sio.chaptalqper>
2019-03-27 14:53:31 +0100

    Merge ModeleDataAccess

--- src/gestionUtilisateurs/models/dataAccess/ConnectDAO.java
+++ src/gestionUtilisateurs/models/dataAccess/ConnectDAO.java
@@ -1,8 +1,13 @@
 package gestionUtilisateurs.models.dataAccess;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Représente une connection à la base de données
@@ -48,6 +53,44 @@ public class ConnectDAO {
 	{
 		user = userName;
 		passwd = userPassword;
+	}
+	
+	public static void generateJDBCUrl() {
+		String urlGeneree = "";
+		String conf_base = "./gestion_utilisateur.conf";
+		HashMap<String, String> mapConfiguration = new HashMap<String, String>();
+		BufferedReader reader;
+		
+		try {
+			// Instanciation du lecteur de fichier
+			// Ouverture du fichier de configuration en lecture
+			reader = new BufferedReader(new FileReader(conf_base));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				String str;
+				// Elimination des commentaires
+				if (!line.contains("#") && !line.contentEquals("")) {
+					// Separation variable/valeur
+					String variableValueArray[];
+					variableValueArray = line.split("=");
+					if (variableValueArray.length > 1) {
+						mapConfiguration.put(variableValueArray[0], variableValueArray[1]);
+					}else {
+						System.err.println("Valeur vide, champ : " + variableValueArray[0]);
+					}
+				}
+								
+				// Lit la prochaine ligne
+				line = reader.readLine();
+			}
+			reader.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		urlGeneree = "jdbc:" + mapConfiguration.get("type_bdd") + "://" + mapConfiguration.get("adresse_bdd") + "/" + mapConfiguration.get("nom_bdd");
+		url = urlGeneree;
 	}
 	
 	/**
